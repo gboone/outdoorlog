23aa1f8b90d056e6c60a67b94ad42868ec13c7a1
Greg Boone <boone.greg@gmail.com>
2016-04-10 12:02:21 -0600

    Rethinks the js loading

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+requirejs.config({
+  baseUrl: '/assets/js/lib',
+});
+
+require(['jquery-2.2.3', 'underscore', 'leaflet'], function (jQuery, _, leaflet) {
+  var activities = $(".activity");
+  total_miles = function() {
+    var total_miles = 0
+    activities.each(function(index) {
+      total_miles += $(this).data('dist')
+    })
+    return total_miles
+  }
+  $("#total-distance").append(total_miles)
+})
+
+
+// if (document.getElementById('activities') != null) {
+//   var activities = JSON.parse(document.getElementById('activities').innerHTML);
+//   document.getElementById('total-distance').innerHTML += total_miles(activities)
+// }
+// if (document.getElementById('parks')) {
+//   var parks = jsonFromURI(document.getElementById('parks').baseURI)
+//   debugger;
+//   parks = dedupeJSON(parks)
+// }
