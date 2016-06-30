40211e9e85a8962aaaecdc34c0de6a9df9757cc8
Greg Boone <gregory.boone@gsa.gov>
2016-06-30 08:23:47 -0600

    Adds maps for individual trail pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,9 @@ requirejs.config({
 });
 
 require(['jquery-2.2.3', 'underscore', 'leaflet'], function (jQuery, _, leaflet) {
-   function onEachFeature(feature, layer) {
+   function markerAnnotation(feature, layer) {
       var properties = feature.properties.activity;
-      var msg = properties.trail + " in " + properties.park + ", " + properties.state;
+      var msg = "<a href='"+ feature.self.url +"'>" + properties.trail + " in " + properties.park + ", " + properties.state + "</a>";
       layer.bindPopup(msg)
    }
     var activities = $(".activity");
@@ -17,45 +17,51 @@ require(['jquery-2.2.3', 'underscore', 'leaflet'], function (jQuery, _, leaflet)
     return total_miles
   }
   function trailAnnotation(feature, layer){
-    var message = feature.properties.Hiking_Name + " ESRI: " + feature.properties.ESRI_OID + " Object: " + feature.properties.OBJECTID;
+    if ( feature.properties.Hiking_Name ) {
+      var message = feature.properties.Hiking_Name + " ESRI: " + feature.properties.ESRI_OID + " Object: " + feature.properties.OBJECTID;
+    } else if ( feature.properties.sht_type ) {
+      var message = "Superior Hiking Trail at " + feature.geometry.coordinates[0]
+    }
     layer.bindPopup(message)
   }
   $("#total-distance").append(total_miles)
-  var Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
-      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
-      minZoom: 0,
-      maxZoom: 20,
-      ext: 'png'
-    });
-  if ($('#parks').attr("type") == "application/json") {
+  
+  if ( $("#map") ) {
+    var Tiles = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
+        minZoom: 0,
+        maxZoom: 20,
+        ext: 'png'
+      });
     var map = new L.Map('map');
-    var latlng = new L.LatLng("44", "-96");
-    map.setView(latlng)
-    map.setZoom(4)
-    map.addLayer(Toner)
-    $.getJSON( $("#parks").attr('src'), function( data ) {
-      L.geoJson(data, {
-        onEachFeature: onEachFeature
-      }).addTo(map);
-    })
-    .fail(function( ) {
-      console.log("Something is wrong. Check the syntax of the JSON at " + $("#parks").attr("src"));
-    })
-  }
-  if ($("#path-data")) {
-    var map = new L.Map('map');
-    var latlng = new L.LatLng(map._container.dataset.lat, map._container.dataset.lon)
-    map.setView(latlng)
-    map.setZoom(14)
-    map.addLayer(Toner)
-    $.getJSON( $("#path-data").attr('src'), function( data ) {
-      L.geoJson(data, {
-        onEachFeature: trailAnnotation
-      }).addTo(map);
-    })
-    .fail(function( ) {
-      console.log("Something is wrong. Check the syntax of the JSON at " + $("#parks").attr("src"));
-    })
+    if ($('#parks').attr("type") == "application/json") {
+      var latlng = new L.LatLng(44, -96);
+      map.setView(latlng)
+      map.setZoom(4)
+      map.addLayer(Tiles)
+      $.getJSON( $("#parks").attr('src'), function( data ) {
+        L.geoJson(data, {
+          onEachFeature: markerAnnotation
+        }).addTo(map);
+      })
+      .fail(function( ) {
+        console.log("Something is wrong. Check the syntax of the JSON at " + $("#parks").attr("src"));
+      })
+    }
+    if ($("#path-data")) {
+      var latlng = new L.LatLng(map._container.dataset.lat, map._container.dataset.lon)
+      map.setView(latlng)
+      map.setZoom(14)
+      map.addLayer(Tiles)
+      $.getJSON( $("#path-data").attr('src'), function( data ) {
+        L.geoJson(data, {
+          onEachFeature: trailAnnotation
+        }).addTo(map);
+      })
+      .fail(function( ) {
+        console.log("Something is wrong. Check the syntax of the JSON at " + $("#parks").attr("src"));
+      })
+    }
   }
 })
 
